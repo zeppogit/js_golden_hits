7f63e3badc8d38a6e04925549afda298afbfc88b
zeppocode@gmail.com <zeppocode@gmail.com>
2021-03-24 17:57:14 -0400

    to test out adding a button to monthly modules to serve as possible link or listener

--- js/project.js
+++ js/project.js
@@ -109,6 +109,9 @@ for (let i = 0; i < 12; i++ ) {
                 <source src="mp3/${hit.recording}" type="audio/mp3">
             </audio> 
         </div>
+        <div class = "lyrics">
+          <button id="${hit.month}" onclick="sendAJAX()">About</button>  
+        </div>
     </div> 
  `; 
 }
